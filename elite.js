b4278e8dde0bcbc27c3b6b07810a37bd0ecf2b7c
Erlend Lind Madsen <erlendf80@gmail.com>
2020-12-11 12:26:09 +0100

    improve jsdoc

--- elite.js
+++ elite.js
@@ -3,11 +3,11 @@
  *
  * Arguments after *tag* can be omitted and replaced with children as the last argument.
  *
- * @param {string} tag
- * @param {string|Array} html
- * @param {object|Array} attributes
- * @param {object|Array} events
- * @param {Array} children
+ * @param {string} tag - the tag name
+ * @param {(string|Array.<Element>)} html - the innerHTML content or children
+ * @param {(Object|Array.<Element>)} attributes - a map of attributes or children
+ * @param {(Object|Array.<Element>)} events - a map of events or children
+ * @param {Array.<Element>} children - the child elements
  */
 function el(tag, ...args) {
   const el = document.createElement(tag)
@@ -24,8 +24,8 @@ function el(tag, ...args) {
 /**
  * Replace the innerHTML of a given parent element with either a single element or an Array of elements.
  *
- * @param {Element|Array} element
- * @param {Element|string} parent
+ * @param {(Element|Array.<Element>)} element - the element or array of elements we wants to set
+ * @param {(Element|string)} parent - the parent element or its id we want to set element to
  */
 function set(element, parent) {
   const el = typeof parent === "string" ? document.getElementById(parent) : parent
